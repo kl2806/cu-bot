86fc76de73636e7f66e7ae0e66f24c2686a8eef4
Kevin Lin <kl2806@columbia.edu>
2016-05-13 23:54:50 -0400

    deploy to heroku

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+
 
 app.get('/webhook', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-      console.log('Example app listening on port 3000!');
+// app.listen(3000, function () {
+//       console.log('Example app listening on port 3000!');
+// });
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
 });
 
