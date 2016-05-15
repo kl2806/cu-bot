97a8abd3cd29677fd260354c966fde68e1cbd88b
Howon <howonbyun@gmail.com>
2016-05-15 13:33:05 -0700

    maybe this 2

--- app.js
+++ app.js
@@ -49,6 +49,21 @@ app.post('/webhook', function (req, res) {
   res.sendStatus(200);
 });
 
+app.post('/webhook/', function (req, res) {
+  // messaging_events = req.body.entry[0].messaging;
+  // for (i = 0; i < messaging_events.length; i++) {
+  //   event = req.body.entry[0].messaging[i];
+  //   sender = event.sender.id;
+  //   if (event.message && event.message.text) {
+  //     text = event.message.text;
+  //     // Handle a text message from this sender
+  //     sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+  //   }
+  // }
+  console.log("here1")
+  res.sendStatus(200);
+});
+
 var port = process.env.PORT || 3000;
 
 var listener = server.listen(port, function() {
