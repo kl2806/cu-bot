ffc648f0cabf8eb204324748b46f5e0e8d962312
Howon <howonbyun@gmail.com>
2016-05-15 12:48:00 -0700

    post handling

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ app.get('/webhook', function (req, res) {
 });
 
 app.get('/webhook', function (req, res) {
-  res.send('Home');
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      res.send(text);
+    }
+  }
+  res.sendStatus(200);
 });
 
 var port = process.env.PORT || 3000;
