b0dcde9772a1c23414c80001f4b6f67455bb3a1e
Howon <howonbyun@gmail.com>
2016-05-15 12:31:45 -0700

    webhook response

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 var http = require('http');
-var express = require('express') //.createServer(); // 
+var express = require('express');
 var app = express();
 var server = http.createServer(app);
 
 
 app.get('/webhook', function (req, res) {
-  res.send('Hello World!');
+  if (req.query['hub.verify_token'] === 'verify') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
 });
 
 app.get('/webhook', function (req, res) {
   res.send('Home');
 });
 
-// app.listen(3000, function () {
-//       console.log('Example app listening on port 3000!');
-// });
-
-
 var port = process.env.PORT || 3000;
+
 var listener = server.listen(port, function() {
   var host = server.address().address;
   console.log('PCompass app listening at http://%s:%s', host, port);
