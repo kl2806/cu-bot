ef0777a6a19e3f221bb40ca78162d117955a3e60
Kevin Lin <kl2806@columbia.edu>
2016-05-16 00:06:49 -0400

    toString

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
+    sender = event.sender.id.toString();
     console.log(sender);
     if (event.message && event.message.text) {
       text = event.message.text;
@@ -56,24 +56,11 @@ app.post('/webhook/', function (req, res) {
   res.sendStatus(200);
 });
 
-// app.post('/webhook/', function (req, res) {
-//   // messaging_events = req.body.entry[0].messaging;
-//   // for (i = 0; i < messaging_events.length; i++) {
-//   //   event = req.body.entry[0].messaging[i];
-//   //   sender = event.sender.id;
-//   //   if (event.message && event.message.text) {
-//   //     text = event.message.text;
-//   //     // Handle a text message from this sender
-//   //     sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
-//   //   }
-//   // }
-//   console.log("here1")
-//   res.sendStatus(200);
-// });
+
 
 var port = process.env.PORT || 3000;
 
 var listener = server.listen(port, function() {
   var host = server.address().address;
-  console.log('PCompass app listening at http://%s:%s', host, port);
+  console.log('Columbia Bot listening at http://%s:%s', host, port);
 });
